938e0fd7d37443d70f2c0e0a0f6f8c4c05879d34
AntoninoIrrera <irrera.antonio2003@gmail.com>
2023-01-24 15:10:54 +0100

    utente inserisce paragrafo e nome da censurare, stampo a schermo paragrafo originale con lunghezza e paragrafo censurato con nuova lunghezza

--- censura.php
+++ censura.php
@@ -1,4 +1,16 @@
+
 <?php
 
 
+    $paragrafo = $_GET["paragrafo"];
+    $censura = $_GET["censura"];
+
+    echo $paragrafo." ".strlen($paragrafo);
+    
+?>
+<br>
+<?php
+    $paragrafoCensurato = str_replace($censura,"***",$paragrafo);
+
+    echo $paragrafoCensurato." ".strlen($paragrafoCensurato);
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Badwords</title>
 </head>
+
 <body>
+
+    <form action="censura.php" method="GET">
+
+        <label for="paragrafo">Inserisci il tuo paragrafo</label>
+        <input type="text" id="paragrafo" name="paragrafo">
+        <label for="censura">Inserisci la parola da censurare</label>
+        <input type="text" id="censura" name="censura">
+        <button type="submit">Invia</button>
+
+    </form>
+
     
+
 </body>
+
 </html>
